f2cc98dd16299d7c7e730b8376eaba16e75f3188
Jakub Szcześniak <115459426+grysik666@users.noreply.github.com>
2022-12-12 10:47:30 +0100

    Wgranie pliku test

---
+++ test.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+import unittest as u
+import kalkulator
+
+class Testy(unittest.TestCase):
+    
+    def test_typ_danych(self):
+        #given
+        a=5
+        b=10
+        znak="+"
+        expected_type=int
+        #when
+        result=kalk(a,b,znak)
+        #then
+        self.assertEqual(type(result),expected_type)
+
+    def test_dodawanie(self):
+        #given
+        a=6
+        b=3
+        expected=9
+        #when
+        result=kalk(a,b,'+')
+        #then
+        self.assertEqual(result,expected)
+    
+    def test_mnozenie(self):
+        #given
+        a=6
+        b=3
+        expected=18
+        #when
+        result=kalk(a,b,'*')
+        #then
+        self.assertEqual(result,expected)
+    
+    def test_odejmowanie(self):
+        #given
+        a=6
+        b=3
+        expected=3
+        #when
+        result=kalk(a,b,'-')
+        #then
+        self.assertEqual(result,expected)
+    
+    def test_dzielenie(self):
+        #given
+        a=6
+        b=3
+        expected=2
+        #when
+        result=kalk(a,b,'/')
+        #then
+        self.assertEqual(result,expected)
+    
+if __name__ == '__main__':
+    unittest.main()
+    
